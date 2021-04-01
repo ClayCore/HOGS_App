dfdd35e26a1dfeffbc94b2c98fe9f6daed6eb7a5
lunarium <claymorealt@op.pl>
2021-04-01 15:12:21 +0200

    Fix server routing

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const port = process.env.port || 3000;
 
 const app = express();
+const rootPath = path.join(__dirname, '..', 'public');
 
-app.use(express.static(__dirname + 'public/'));
-app.use(express.static('build/static'));
+app.use(express.static(rootPath));
+app.use(express.static('/build/static'));
 
 app.listen(port, () => {
 	console.log(`Server is listening at [https://localhost:${port}]`);
