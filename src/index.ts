f183c63888e74f9e46d79fc3cbc315b5a6d56dae
junior <>
2021-04-02 23:14:18 +0200

    improve project directory structure

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+export default function hook(app: express.Express) {
+	app.get('/', (_, res) => res.sendFile('index.html', { root: './build' }));
+	app.get('/ban_appeal', (_, res) => res.sendFile('ban_appeal.html', { root: './build' }));
+	app.get('/contact', (_, res) => res.sendFile('contact.html', { root: './build' }));
+	app.get('/feedback', (_, res) => res.sendFile('feedback.html', { root: './build' }));
+	app.get('/home', (_, res) => res.sendFile('home.html', { root: './build' }));
+
+	app.use(express.static('./build'));
+}
