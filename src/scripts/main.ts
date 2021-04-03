1fff4bf1afd3bce132076aec17b32512af529c81
lunarium <claymorealt@op.pl>
2021-04-03 11:01:38 +0200

    Add functional topbar and fix scss compile

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,13 +1,4 @@
-import * as serviceWorker from './serviceWorker';
-
 import { initFontLibrary } from './utils';
-
-const ENV = process.env.NODE_ENV;
-if (ENV) {
-	serviceWorker.register();
-} else {
-	serviceWorker.unregister();
-}
 
 (function () {
 	require('../styles/master.scss');
