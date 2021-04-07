97e3bdccc29643cba2f4ea5761e3e838c7d362be
lunarium <claymorealt@op.pl>
2021-04-07 09:44:48 +0200

    Update contact page to include fetched info

--- server/src/routes/homepage.ts
+++ server/src/routes/homepage.ts
@@ -55,25 +55,28 @@ homepage.route('/').get(async (req, res) => {
 	const apiKey = getApiKey();
 
 	const sids = [
-		{ sid: '76561198066378373', name: 'Fuel-Black', role: 'Owner' },
-		{ sid: '76561197962534841', name: 'Stfwn', role: 'Administration' },
-		{ sid: '76561198115627631', name: 'Claymore', role: 'Developer' },
-		{ sid: '76561198869806001', name: 'Junior', role: 'Developer' },
-		{ sid: '76561198148565659', name: 'Fl1p', role: 'Moderation' },
-		{ sid: '76561198429396241', name: 'Skar', role: 'Moderation' },
-		{ sid: '76561198316733348', name: 'Rechurd', role: 'Developer' },
+		{ sid: '76561198066378373', name: 'Fuel-Black', role: 'Owner', avatar_url: '' },
+		{ sid: '76561197962534841', name: 'Stfwn', role: 'Administration', avatar_url: '' },
+		{ sid: '76561198115627631', name: 'Claymore', role: 'Developer', avatar_url: '' },
+		{ sid: '76561198869806001', name: 'Junior', role: 'Developer', avatar_url: '' },
+		{ sid: '76561198148565659', name: 'Fl1p', role: 'Moderation', avatar_url: '' },
+		{ sid: '76561198429396241', name: 'Skar', role: 'Moderation', avatar_url: '' },
+		{ sid: '76561198316733348', name: 'Rechurd', role: 'Moderation', avatar_url: '' },
 	];
 
-	// Destructure and pull out the useful stuff out.
-	const roles = sids.map((x) => x.role);
-	const names = sids.map((x) => x.name);
 	const avatarUrls = await Promise.all(
 		sids.map((x) => x.sid).map((steamid: string) => getUserAvatar(steamid, apiKey))
 	);
 
+	// Add all the values back into the array
+	// TODO: Refactor cuz this seems stupid
+	for (let i = 0; i < sids.length; ++i) {
+		sids[i].avatar_url = avatarUrls[i];
+	}
+
 	// NOTE: this renders slowly since the entire routing is paused while the avatar urls are being fetched
 	// TODO: preload avatars or add a loader?
-	res.render('index.pug', { staffAvatars: avatarUrls, staffNames: names, staffRoles: roles });
+	res.render('index.pug', { staffInfo: sids });
 });
 
 export default homepage;
