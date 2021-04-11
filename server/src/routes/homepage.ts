67c12287fe091f48cac2e1fce2cbbf13a87bd798
lunarium <claymorealt@op.pl>
2021-04-11 20:41:06 +0200

    Change formatting

--- server/src/routes/homepage.ts
+++ server/src/routes/homepage.ts
@@ -6,8 +6,8 @@ const homepage = express.Router();
 homepage.route('/').get(async (req, res) => {
 	// NOTE: this renders slowly since the entire routing is paused while the avatar urls are being fetched
 	// TODO: preload avatars or add a loader?
-	const staffInfo =await fetchStaff(STAFF_MEMBERS);
-	res.render('index.pug', { staffInfo: staffInfo});
+	const staffInfo = await fetchStaff(STAFF_MEMBERS);
+	res.render('index.pug', { staffInfo: staffInfo });
 });
 
 export default homepage;
--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -9,17 +9,17 @@ async function fetchProfileInfo(steamID64: string) {
 	return {
 		name: data.profile.steamID,
 		avatar: data.profile.avatarFull[0],
-	}
+	};
 }
 
 async function fetchStaff(staff: Array<User>) {
-	const promises = staff.map(member => fetchProfileInfo(member.steamid));
+	const promises = staff.map((member) => fetchProfileInfo(member.steamid));
 	const profiles = await Promise.all(promises);
 	return staff.map((member, i) => ({
 		avatarUrl: profiles[i].avatar,
 		nick: member.nick,
 		role: member.role,
-	}))
+	}));
 }
 
-export default fetchStaff
+export default fetchStaff;
