1a8c24108fcb06d0f0774dce168a6534da125282
lunarium <claymorealt@op.pl>
2021-04-13 08:12:30 +0200

    Add stub definitions for first draft of player stats page

--- server/src/routes/stats.ts
+++ server/src/routes/stats.ts
@@ -1,8 +1,20 @@
+import { TF_GAMESERVER } from '../utils/constants';
 import express from 'express';
+import gamedig from 'gamedig';
+
+const queryPlayers = async () => {
+	const playersState = await gamedig.query(TF_GAMESERVER as gamedig.QueryOptions);
+
+	return playersState.players;
+};
 
 const stats = express.Router();
 stats.route('/stats').get((req, res) => {
 	res.render('stats/index.pug');
+});
+
+stats.route('/api/stats').get((req, res) => {
+	res.render('stats/players-table.pug', { count: 6 });
 });
 
 export default stats;
