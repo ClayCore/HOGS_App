8fe1846f1c209b7b1f4b63e444cf9b0228e81ba6
lunarium <claymorealt@op.pl>
2021-04-14 17:57:06 +0200

    Add comment explaining the fetch operation on the client

--- client/src/script/base.ts
+++ client/src/script/base.ts
@@ -3,6 +3,10 @@ import { $ } from './utils.js';
 (function () {
 	// Make sure the DOM is loaded
 	window.addEventListener('load', async () => {
+		// TODO: Check for the subpage containing `/stats`
+		// This eliminates the need for paranoically checking
+		// every subgpage and root homepage for the necessary div to render
+		// the data fetched from the server into
 		const playersTable = $('#playersTable');
 
 		if (playersTable) {
